05d98fcce13d262284e7b1331687ce796e1a10ef
Deirdre Bringas <deirdrebringas@gmail.com>
2017-03-20 13:41:40 +0000

    adding part 2

--- src/Receiver2a.java
+++ src/Receiver2a.java
@@ -1,7 +1,87 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 /**
- * Created by deirdrebringas on 14/03/2017.
+ * Created by s1368635 on 14/03/2017.
  */
 public class Receiver2a {
+    private static void receive(int portNum, String filename) throws IOException {
+        try {
+            //create sockets for both receiver and sender
+            DatagramSocket recSocket = new DatagramSocket(portNum);
+            DatagramSocket sendSocket = new DatagramSocket();
+
+            File file = new File(filename);
+            FileOutputStream out = new FileOutputStream(file);
+            short packNum = 0;
+
+            boolean fileReceived = false;
+            //initialised so that lastPackNum == pacKNum is false; number cannot be 2
+            int lastPackNum = 2;
+
+            System.out.println("Receiving file...");
+
+            while (!fileReceived) {
+                //File is 1024 bytes plus 3 for offset
+                byte[] buffer = new byte[1027];
+                DatagramPacket recPacket = new DatagramPacket(buffer, buffer.length);
+                recSocket.receive(recPacket);
+                byte[] data = recPacket.getData();
+
+                //Real file starts at data[3] because of an offset of 3
+                byte[] offset = Arrays.copyOfRange(data, 0, 2);
+                packNum = ByteBuffer.wrap(offset).getShort();
+
+                System.out.println("Sending Acknowledgement");
+                //Attempting to send acknowledgements
+
+                //if packet has already been seen
+                if (lastPackNum >= packNum) {
+                    InetAddress recPackAddress = recPacket.getAddress();
+                    sendAck(packNum, portNum, recSocket, recPackAddress);
+
+                //check if packet is next in sequence
+                } else if (lastPackNum + 1 == packNum) {
+                    //Check flag
+                    int lastPack = (int) data[2];
+                    out.write(buffer, 3, data.length-3);
+
+                    //Check if the flag in the data received is 1, ie is the last packet
+                    if (lastPack == 1) {
+                        fileReceived = true;
+                        System.out.println("File received.");
+                        //Close output stream and socket
+                        out.close();
+                        recSocket.close();
+                    }
+                    //change lastPackNum for next execution of while loop
+                    lastPackNum = packNum;
+                }
+
+            }
+        } catch (Exception e) {}
+    }
+
+    //new method to send acknowledgement to specified port to sender
+    public static void sendAck(short packNum, int portNum, DatagramSocket socket, InetAddress add) throws IOException {
+        try {
+            ByteBuffer bb = ByteBuffer.allocate(2);
+            byte[] sendByte = bb.putShort(packNum).array();
+            //Strange error if using the same port number as receiving socket so add 1 to change
+            DatagramPacket sendPack = new DatagramPacket(sendByte, sendByte.length, add, portNum + 1);
+            socket.send(sendPack);
+            System.out.println("Acknowledgement Sent!");
+        } catch (IOException e) {}
+    }
+
+    public static void main(String[] args) {
+        int port = Integer.parseInt(args[1]);
+        String filename = args[2];
+    }
 }
--- src/Sender2a.java
+++ src/Sender2a.java
@@ -1,7 +1,184 @@
-package PACKAGE_NAME;
+import com.sun.net.httpserver.*;
+import com.sun.net.httpserver.Authenticator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 /**
- * Created by deirdrebringas on 14/03/2017.
+ * Created by s1368635 on 14/03/2017.
  */
+
+//allowed to transmit multiple packets (when available) without waiting for an acknowledgment
+//maximum of N unacknowledged packets
 public class Sender2a {
+
+    //Used for the while loop in Ack class to stop the run method
+    private static boolean StopAckRun = false;
+
+    public static void send(InetAddress address, int portNum, String filename, int RetryTimeout, int N) throws IOException {
+        long starttime = System.currentTimeMillis();
+        int timeout = RetryTimeout;
+        DatagramSocket socket = new DatagramSocket();
+        System.out.println("Socket connected");
+        //An ArrayList to keep track of the packets that have been ack'ed
+        ArrayList<Packet> ackedPackets = new ArrayList<>();
+        int ackedPackNum = -1;
+        long timer;
+
+        byte[] buffer = new byte[1024];
+        int numBytes = 0;
+
+        try {
+            File file = new File(filename);
+            FileInputStream in = new FileInputStream(file);
+            //Some variables for the while loop
+            short packNum = 0;
+            int packLen;
+            int dataLeft;
+
+            boolean last = false;
+            boolean retransmit = false;
+            boolean loop = true;
+
+            //Starting threat
+            Acknowledge Ack = new Acknowledge(portNum, timeout);
+            System.out.println("Sending packets...");
+
+            while (loop) {
+
+                //if the packet number has been acknowledged, remove that and the previous ones that have been
+                //acknowledged before it so we can "shift" the window
+                while (ackedPackets.size() > 0 && ackedPackets.get(0).packNum <= ackedPackNum) {
+                    ackedPackets.remove(0);
+
+                }
+
+                //check if we are still within the window size for the list of acknowledged packets
+                while (!last && ackedPackets.size() < N) {
+                    dataLeft = in.available();
+                    if (dataLeft > buffer.length) {
+                        packLen = buffer.length;
+                    } else {
+                        //Check if last packet
+                        packLen = dataLeft;
+                        last = true;
+                        //Start timer after finishing last iteration of loop
+                        timer = System.currentTimeMillis();
+                    }
+                    numBytes += packLen;
+
+                    //Full size of the file including the header
+                    byte[] bytes = new byte[1027];
+                    in.read(bytes, 3, packLen);
+
+                    //get the header of the current packet
+                    byte[] header = ByteBuffer.allocate(2).putShort(packNum).array();
+                    bytes[0] = header[0];
+                    bytes[1] = header[1];
+
+                    //If this is the last packet then make sure the flag is 1, if not then flag is 0
+                    //This is so the receiver knows that this is the last packet being sent
+                    if (last) {
+                        bytes[2] = 1;
+                    } else {
+                        bytes[2] = 0;
+                    }
+
+                    DatagramPacket sendPacket = new DatagramPacket(bytes, bytes.length, address, portNum);
+                    socket.send(sendPacket);
+
+                    //add packets sent to acknowledged list
+                    ackedPackets.add(new Packet(packNum, sendPacket, System.currentTimeMillis()));
+                    packNum++;
+                }
+
+                //check whether we need to retransmit packets
+                //If a timeout occurs, the sender resends all packets that have been previously
+                //sent but that have not yet been acknowledged.
+                retransmit = ackedPackets.get(0).timeSent + timeout <= System.currentTimeMillis();
+                if (ackedPackets.size() > 0 && retransmit) {
+                    for (int i = 0; i < ackedPackets.size(); i ++) {
+                        Packet currPack = ackedPackets.get(i);
+                        //resend data and store back into array list
+                        socket.send(currPack.data);
+                        currPack.timeSent = System.currentTimeMillis();
+                        ackedPackets.set(i, currPack);
+                    }
+                }
+                if (last) {
+                    loop = false;
+                    StopAckRun = false;
+                    socket.close();
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        final InetAddress host = InetAddress.getByName(args[0]); //localhost
+        int port = Integer.parseInt(args[1]);
+        String filename = args[2];
+        int timeout = Integer.parseInt(args[3]);
+        int N = Integer.parseInt(args[4]);
+
+        send(host, port, filename, timeout, N);
+
+    }
+
+
+    //Create Ack class that implements Runnable interface to create thread
+    private static class Acknowledge implements Runnable {
+        DatagramSocket recSocket;
+        int timeout;
+        Thread t;
+
+        public Acknowledge(int portNum, int timeout) throws SocketException {
+            //Create different socket for receiver in different port
+            //strange error when using same port number so add 1 to change port num
+            this.recSocket = new DatagramSocket(portNum+1);
+            this.timeout = timeout;
+            this.t = new Thread(this);
+        }
+        @Override
+        public void run() {
+            t.start();
+            while (!StopAckRun) {
+                //Allocate two bytes
+                byte[] recBuf = new byte[2];
+                DatagramPacket recPacket = new DatagramPacket(recBuf, recBuf.length);
+                //Set how many milliseconds until the socket times out
+                try {
+                    recSocket.setSoTimeout(timeout);
+                    recSocket.receive(recPacket);
+                    byte[] recData = recPacket.getData();
+                } catch (SocketTimeoutException e){
+                    System.out.println(e);
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+                //yield its current use of a processor
+                t.yield();
+            }
+
+        }
+    }
+
+    //Create Packet class that holds the information we need for acknowledgement
+    private static class Packet {
+        int packNum;
+        DatagramPacket data;
+        long timeSent;
+        public Packet(int num, DatagramPacket data, long time) {
+            this.packNum = num;
+            this.data = data;
+            this.timeSent = time;
+        }
+    }
 }
